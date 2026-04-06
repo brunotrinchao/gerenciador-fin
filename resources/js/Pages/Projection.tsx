aa8334045438a07914081d90cd0b679bb4d863c5
brunosouza <bruno.souza@onfly.com.br>
2026-04-06 17:36:22 -0300

    Change grid mobile projection

--- resources/js/Pages/Projection.tsx
+++ resources/js/Pages/Projection.tsx
@@ -202,7 +202,7 @@ export default function Projection({
                 </div>
 
                 {/* Summary cards */}
-                <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+                <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
                     <SummaryCard
                         label="Saldo Atual"
                         value={currentBalance}
